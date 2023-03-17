21383af4bda83c55113ebdc7f25a4156545967c7
ddddreem <ddddreem@qq.com>
2023-03-17 10:56:24 +0800

    项目构建...

---
+++ src/test/java/org/dp/smartcampus/SmartcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dp.smartcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
